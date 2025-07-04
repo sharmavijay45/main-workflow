850eb21bda941b9d4995c73c411432cdb46eea8a
sharmavijay45 <blackholeinfiverse45@gmail.com>
2025-07-04 15:34:45 +0530

    Update Notification.js

--- server/models/Notification.js
+++ server/models/Notification.js
@@ -8,7 +8,7 @@ const NotificationSchema = new mongoose.Schema({
   },
   type: {
     type: String,
-    enum: ["task_assigned", "task_submitted", "submission_approved", "submission_rejected"],
+    enum: ["task_assigned", "task_submitted", "submission_approved", "submission_rejected","submission_reviewed"],
     required: true,
   },
   title: {
